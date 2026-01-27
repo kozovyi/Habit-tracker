74222e65db6655acd7fbd5411409ac3543e08fa2
Pavlo Kozovyi <p.kozoviy@gmail.com>
2026-01-27 15:06:47 +0200

    docs(docs): add swagger interactive documentation

--- backend/src/main/urls.py
+++ backend/src/main/urls.py
@@ -16,12 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     # admin
     path("admin/", admin.site.urls),
+    
+    # swagger
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     
     # auth / user
     path("api/v1/auth/", include('djoser.urls')),
