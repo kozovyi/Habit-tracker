63f2f8ea6bc1aabe57bc237f15c49393e345b13c
Pavlo Kozovyi <p.kozoviy@gmail.com>
2026-01-27 16:36:47 +0200

    db(activities): create Activity and ActivityLog models

--- backend/src/activity/models.py
+++ backend/src/activity/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+import uuid
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class Activity(models.Model):
+    activity_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_activities",
+        related_query_name="activities"
+    )
+    name = models.CharField(max_length=255)
+    descriprion = models.TextField(blank=True, null=True)
+    is_archived = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Activities"
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.user.email})"
+    
+
--- backend/src/activity_log/models.py
+++ backend/src/activity_log/models.py
@@ -1,3 +1,44 @@
-from django.db import models
 
-# Create your models here.
+import uuid
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from activity.models import Activity
+
+
+User = get_user_model()
+
+class ActivityLog(models.Model):
+    class ActivityLogStatus(models.TextChoices):
+        COMPLETED = 'Виконано'
+        PARTIAL = 'Частково'
+        SKIPPED = 'Пропущено'
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="user_avtivities_logs",
+    )
+    activity = models.ForeignKey(
+        to=Activity, 
+        on_delete=models.CASCADE, 
+        related_name="logs"
+    )
+
+    date = models.DateField()
+    status = models.CharField(max_length=15, choices=ActivityLogStatus.choices, default=ActivityLogStatus.COMPLETED)
+
+    value = models.PositiveIntegerField(default=0, blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('activity', 'date')
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"{self.activity.name} log for {self.date}"
